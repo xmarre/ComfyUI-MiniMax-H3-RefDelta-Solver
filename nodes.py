6cdc7cc7f4d349bf4cd10fa804f7b54d6802a59b
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-24 17:50:08 +0200

    Fix ComfyUI custom-node package import (#3)
    * Fix custom-node package-relative import
    
    * Test isolated ComfyUI custom-node import

--- nodes.py
+++ nodes.py
@@ -1,4 +1,6 @@
-from comfyui_refdelta_solver.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+if __package__:
+    from .comfyui_refdelta_solver.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+else:
+    from comfyui_refdelta_solver.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-
